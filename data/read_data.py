e86f7ac57f31bab685b37dd923987aea11fb3d99
paupinger <paulsemmelrock@gmail.com>
2019-11-23 22:06:47 +0100

    changed gitignore and created read_data.py

---
+++ data/read_data.py
@@ -0,0 +1,5 @@
+from mlxtend.data import loadlocal_mnist
+
+X_full, y_full = loadlocal_mnist(
+                                 images_path='../data/t10k-images-idx3-ubyte',
+                                 labels_path='../data/t10k-labels-idx1-ubyte')
